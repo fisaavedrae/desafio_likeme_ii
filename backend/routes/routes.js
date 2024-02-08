491da12db79f9909f74f114aff37dabb71bf8511
Felipe Saavedra <fisaavedrae@gmail.com>
2024-02-07 21:11:05 -0300

    first commit

---
+++ backend/routes/routes.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const router = express.Router();
+const { getPosts, addPost, editPost, deletePost } = require("../../database/consultas.js");
+const { isValidUrl } = require("../../server/validation/validation");
+
+
+router.get('/posts', async (req, res) => {
+    try {
+        const results = await getPosts()
+        res.json(results)
+    }
+    catch (error) {
+        res.status(400).json({ message: 'Error al obtener los posts' })
+    }
+
+})
+
+router.post('/posts', async (req, res) => {
+
+    const { titulo, imgSrc, descripcion } = req.body
+    if (!titulo || !imgSrc || !descripcion) {
+        res.status(400).json({ message: 'Debe ingresar toda la información' })
+        return
+    } else {
+        if (!isValidUrl(imgSrc)) {
+            res.status(400).json({ message: 'Debe ingresar una URL valida' })
+            return
+        } else {
+            try {
+                await addPost(titulo, imgSrc, descripcion, 0)
+                res.status(200).json({ message: 'Post agregado' })
+            }
+            catch (error) {
+                res.status(400).json({ message: 'Error al agregar el post' })
+            }
+
+        }
+    }
+})
+
+router.put('/posts/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+        await editPost(id)
+        res.status(200).json({ message: 'Like agregado' })
+    }
+    catch (error) {
+        res.status(400).json({ message: 'Error al agregar el like' })
+    }
+})
+
+router.delete('/posts/:id', async (req, res) => {
+
+    const { id } = req.params
+    try {
+        await deletePost(id)
+        res.status(200).json({ message: 'Post eliminado' })
+    }
+    catch (error) {
+        res.status(400).json({ message: 'Error al eliminar el post' })
+    }
+})
+
+
+
+module.exports = router;
