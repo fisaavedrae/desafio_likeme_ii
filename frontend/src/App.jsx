89e44f3b748ca2ec59aa8e7a9702b483338bf87b
Felipe Saavedra <fisaavedrae@gmail.com>
2024-02-09 02:12:45 -0300

    css final

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -91,7 +91,7 @@ function App() {
 
   return (
     <div className="App">
-      <h2 className="py-5 text-center logo"> Like Me</h2>
+      <h2 className="py-5 text-center logo"> Like Me <i className="fa-solid fa-heart"></i></h2>
       <div className="row m-auto px-5">
         <div className="col-12 col-sm-4">
           <Form
